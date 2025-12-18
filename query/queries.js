6c69ecf49d355aa026291be649697771c1352493
cristhianchulca49 <cristhianchulca49@gmail.com>
2025-12-18 13:18:13 +0100

    feat: add 5 queries

--- query/queries.js
+++ query/queries.js
@@ -1,17 +1,47 @@
 // 1. Mostrar tots els documents en la col·lecció Restaurants.
-
+db.restaurants.find()
 
 // 2. Mostrar el restaurant_id i name per tots els documents en la col·lecció Restaurants.
-
+db.restaurants.find(
+    {},
+    {
+        _id: 0,
+        restaurant_id: 1,
+        name: 1
+    }
+)
 
 // 3. Mostrar el restaurant_id, name, borough i cuisine.
-
+db.restaurants.find(
+    {},
+    { 
+        _id: 0,
+        restaurant_id: 1, 
+        name: 1, 
+        borough: 1,
+        cuisine: 1 
+    }
+)
 
 // 4. Mostrar restaurant_id, name, borough i zip code.
-
+db.restaurants.find(
+  {}, 
+  { 
+    _id: 0, 
+    restaurant_id: 1, 
+    name: 1, 
+    borough: 1, 
+    "address.zipcode": 1 
+  }
+)
 
 // 5. Mostrar tots els restaurants que estan en el Bronx.
-
+db.restaurants.find(
+    {},
+    {
+        borough: "Bronx"
+    }
+)
 
 // 6. Mostrar els primers 5 restaurants que estan en el Bronx.
 
